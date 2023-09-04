e160bf42ad3c2482e393753f714d8ebfbc72445f
Daniel Robinson <dnlglnnrbnsn@gmail.com>
2023-09-04 05:27:48 -0600

    Added settings menu allowing changing waveform, enabling power plot mode, and trigger

--- MicroPython Code/main.py
+++ MicroPython Code/main.py
@@ -4,41 +4,104 @@ import data
 from ST7735 import TFT
 from sysfont import sysfont
 
+def startTimers():
+    global dacTimer, measurementTimer, plottingTimer
+    dacTimer = Timer(mode=Timer.PERIODIC, period=1, callback=dacHandler)
+    measurementTimer = Timer(mode=Timer.PERIODIC, period=10, callback=measurementHandler)
+    plottingTimer = Timer(mode=Timer.PERIODIC, period=10, callback=plottingHandler)
+
+def stopTimers():
+    global dacTimer, measurementTimer, plottingTimer
+    dacTimer.deinit()
+    measurementTimer.deinit()
+    plottingTimer.deinit()
+
 def dacHandler(timer):
-    global dacStep, sineDAC
-    sineDAC.duty_u16(int(sine[dacStep]))
+    global dacStep, waveDAC
+    waveformSetting = settings[0][1][settings[0][0]]
+    if(waveformSetting == "sine"):
+        waveDAC.duty_u16(int(sine[dacStep]))
+    elif(waveformSetting == "triangle"):
+        waveDAC.duty_u16(int(tri[dacStep]))
+    elif(waveformSetting == "square"):
+        if (dacStep < 500):
+            waveDAC.duty_u16(65535)
+        else:
+            waveDAC.duty_u16(32768)
     if (dacStep != 999):
         dacStep += 1
     else:
         dacStep = 0
 
 def measurementHandler(timer):
-    global cursorPos, voltageDataC1, voltageDataC2, yValsC1, yValsC2, tft, measuring, start
-    if(measuring):
-        valSumC1 = 0
-        valSumC2 = 0
-        for y in range(100):
-            valSumC1 += C1.read_u16() - C1Ref.read_u16()
-            valSumC2 += C2.read_u16() - C2Ref.read_u16()
-        voltageDataC1[cursorPos] = voltageFactor*(valSumC1*0.01)
-        voltageDataC2[cursorPos] = voltageFactor*(valSumC2*0.01)
-        yValsC1[cursorPos] = int(69-displayFactor*voltageDataC1[cursorPos])
-        yValsC2[cursorPos] = int(69-displayFactor*voltageDataC2[cursorPos])
-        tft.pixel((cursorPos, yValsC2[cursorPos]),TFT.BLUE)
-        tft.pixel((cursorPos, yValsC1[cursorPos]),TFT.RED)
-        if(cursorPos == 159):
-            start = False
-            measuring = False
-            moveCursor(0)
-        else:
-            cursorPos += 1
+    global cursorPos, voltageDataC1, voltageDataC2, yValsC1, yValsC2, ADCDataC1, ADCDataC2, tft, measuring, currentArrayIndex
+    valSumC1 = 0
+    valSumC2 = 0
+    for y in range(100):
+        valSumC1 += C1.read_u16() - C1Ref.read_u16()
+        valSumC2 += C2.read_u16() - C2Ref.read_u16()
+    ADCDataC1[currentArrayIndex] = valSumC1*0.01
+    ADCDataC2[currentArrayIndex] = valSumC2*0.01
+    currentArrayIndex += 1
+    if (currentArrayIndex == 160):
+        currentArrayIndex = 0
+
+def plottingHandler(timer):
+    global voltageDataC1, voltageDataC2, yValsC1, yValsC2, ADCDataC1, ADCDataC2, tft, currentArrayIndex, plottingIndex, measuring, cursorPos
+    if (plottingIndex == 0 and settings[2][1][settings[2][0]] == "wave out peak" and not (dacStep > 445 and dacStep < 455)):
+        plottingIndex = 0
+    elif(plottingIndex < 160):
+        cursorPos = plottingIndex
+        if(settings[1][1][settings[1][0]] == "volts"):
+            if(currentArrayIndex >= 20):
+                voltageDataC1[plottingIndex] = voltageFactor*(ADCDataC1[currentArrayIndex-20])
+                voltageDataC2[plottingIndex] = voltageFactor*(ADCDataC2[currentArrayIndex-20])
+                yValsC1[plottingIndex] = int(69-displayFactor*voltageDataC1[plottingIndex])
+                yValsC2[plottingIndex] = int(69-displayFactor*voltageDataC2[plottingIndex])
+                
+                tft.pixel((plottingIndex, yValsC2[plottingIndex]),TFT.BLUE)
+                tft.pixel((plottingIndex, yValsC1[plottingIndex]),TFT.RED)
+            else:
+                voltageDataC1[plottingIndex] = voltageFactor*(ADCDataC1[currentArrayIndex+140])
+                voltageDataC2[plottingIndex] = voltageFactor*(ADCDataC2[currentArrayIndex+140])
+                yValsC1[plottingIndex] = int(69-displayFactor*voltageDataC1[plottingIndex])
+                yValsC2[plottingIndex] = int(69-displayFactor*voltageDataC2[plottingIndex])
+                
+                tft.pixel((plottingIndex, yValsC2[plottingIndex]),TFT.BLUE)
+                tft.pixel((plottingIndex, yValsC1[plottingIndex]),TFT.RED)
+        else: #power plot mode
+            if(currentArrayIndex >= 20):
+                voltageDataC1[plottingIndex] = voltageFactor*(ADCDataC1[currentArrayIndex-20])*voltageFactor*(ADCDataC2[currentArrayIndex-20])
+                yValsC1[plottingIndex] = int(69-(1/3)*displayFactor*voltageDataC1[plottingIndex])
+                
+                tft.pixel((plottingIndex, yValsC1[plottingIndex]),TFT.PURPLE)
+            else:
+                voltageDataC1[plottingIndex] = voltageFactor*(ADCDataC1[currentArrayIndex+140])*voltageFactor*(ADCDataC2[currentArrayIndex+140])
+                yValsC1[plottingIndex] = int(69-(1/3)*displayFactor*voltageDataC1[plottingIndex])
+                
+                tft.pixel((plottingIndex, yValsC1[plottingIndex]),TFT.PURPLE)
+        plottingIndex+=1
+        if(plottingIndex == 160):
+            cursorPos = 0
+    else:
+        measuring=False
 
 def updateText():
     global tft
     tft.text((0, 0), '{:.2f}S'.format(cursorPos*0.01), TFT.GREEN, sysfont, 1, nowrap=True)
-    tft.text((40, 0), '{:+.2f}V'.format(voltageDataC1[cursorPos]), TFT.RED, sysfont, 1, nowrap=True)
-    tft.text((90, 0), '{:+.2f}V'.format(voltageDataC2[cursorPos]), TFT.BLUE, sysfont, 1, nowrap=True)
-    tft.text((140, 0), 'SIN', TFT.YELLOW, sysfont, 1, nowrap=True)
+    if(settings[1][1][settings[1][0]] == "volts"):
+        tft.text((40, 0), '{:+.2f}V'.format(voltageDataC1[cursorPos]), TFT.RED, sysfont, 1, nowrap=True)
+        tft.text((90, 0), '{:+.2f}V'.format(voltageDataC2[cursorPos]), TFT.BLUE, sysfont, 1, nowrap=True)
+    else:
+        tft.text((40, 0), '{:+.2f}mW'.format(voltageDataC1[cursorPos]), TFT.PURPLE, sysfont, 1, nowrap=True)
+    
+    waveformSetting = settings[0][1][settings[0][0]]
+    if(waveformSetting == "sine"):
+        tft.text((140, 0), 'SIN', TFT.YELLOW, sysfont, 1, nowrap=True)
+    elif(waveformSetting == "triangle"):
+        tft.text((140, 0), 'TRI', TFT.YELLOW, sysfont, 1, nowrap=True)
+    elif(waveformSetting == "square"):
+        tft.text((140, 0), 'SQR', TFT.YELLOW, sysfont, 1, nowrap=True)
 
 def drawAxes():
     tft.text((6, 8), 'v', TFT.YELLOW, sysfont, 1, nowrap=True)
@@ -74,12 +137,77 @@ def moveCursor(newPos):
     global cursorPos
     tft.vline((cursorPos, 10), 118, TFT.BLACK)
     drawAxes()
-    tft.pixel((cursorPos, yValsC2[cursorPos]),TFT.BLUE)
-    tft.pixel((cursorPos, yValsC1[cursorPos]),TFT.RED)
+    if(settings[1][1][settings[1][0]] == "volts"):
+        tft.pixel((cursorPos, yValsC2[cursorPos]),TFT.BLUE)
+        tft.pixel((cursorPos, yValsC1[cursorPos]),TFT.RED)
+    else:
+        tft.pixel((cursorPos, yValsC1[cursorPos]),TFT.PURPLE)
     cursorPos = newPos
     tft.vline((cursorPos, 10), 118, TFT.GREEN)
-    tft.pixel((cursorPos, yValsC2[cursorPos]),TFT.BLUE)
-    tft.pixel((cursorPos, yValsC1[cursorPos]),TFT.RED)
+    if(settings[1][1][settings[1][0]] == "volts"):
+        tft.pixel((cursorPos, yValsC2[cursorPos]),TFT.BLUE)
+        tft.pixel((cursorPos, yValsC1[cursorPos]),TFT.RED)
+    else:
+        tft.pixel((cursorPos, yValsC1[cursorPos]),TFT.PURPLE)
+
+def openMenu():
+    global tft, leftButton, rightButton, leftCounter, rightCounter, selectButton, selectCounter, settings, cursorPos
+    stopTimers()
+    tft.fill(TFT.BLACK)
+    tft.text((0, 0 ), 'Waveform:', TFT.WHITE, sysfont, 1, nowrap=True)
+    tft.text((0, 12), '   Units:', TFT.WHITE, sysfont, 1, nowrap=True)
+    tft.text((0, 24), ' Trigger:', TFT.WHITE, sysfont, 1, nowrap=True)
+    menuIndex = 0
+    updateMenuSelection(menuIndex)
+    while(True):
+        time.sleep(0.001)
+        if (not leftButton.value()):
+            leftCounter += 1
+            if (leftCounter == 5):
+                settings[menuIndex][0] = settings[menuIndex][0] - 1
+                if(settings[menuIndex][0] < 0):
+                    settings[menuIndex][0] = len(settings[menuIndex][1]) - 1
+                updateMenuSelection(menuIndex)
+        else:
+            leftCounter = 0
+    
+        if (not rightButton.value()):
+            rightCounter += 1
+            if (rightCounter == 5):
+                settings[menuIndex][0] = settings[menuIndex][0] + 1
+                if(settings[menuIndex][0] >= len(settings[menuIndex][1])):
+                    settings[menuIndex][0] = 0
+                updateMenuSelection(menuIndex)
+        else:
+            rightCounter = 0
+        
+        if (not selectButton.value()):
+            selectCounter += 1
+            if (selectCounter == 5):
+                menuIndex += 1
+                if menuIndex == len(settings):
+                    if (settings[1][1][settings[1][0]] == "watts"):
+                        tft.fill(TFT.BLACK)
+                        tft.text((0, 0), "Note: Power plot mode on. This mode assumes channel 1 is attached across the  component whose power drawis being measured, and    channel 2 is attached     across a 1 kOhm resistor  placed in series with the component.                                          Press select to continue.", TFT.WHITE, sysfont, 1, nowrap=False)
+                        time.sleep(1)
+                        while(True):
+                            if (not selectButton.value()):
+                                break
+                    break
+                updateMenuSelection(menuIndex)
+        else:
+            selectCounter = 0
+    startTimers()
+    tft.fill(TFT.BLACK)
+    drawAxes()
+    cursorPos = 0
+
+def updateMenuSelection(menuIndex):
+    global tft
+    tft.fillrect((60,12*menuIndex - 4), (99, 14), TFT.BLACK)
+    tft.hline((60, 12*menuIndex + 9), 6*len(settings[menuIndex][1][settings[menuIndex][0]]), TFT.WHITE)
+    for x in range(len(settings)):
+        tft.text((60, x*12), settings[x][1][settings[x][0]], TFT.WHITE, sysfont, 1, nowrap=True)
 
 spi = SPI(1, baudrate=133000000, polarity=0, phase=0, sck=Pin(14), mosi=Pin(15), miso=Pin(12))
 tft=TFT(spi,10,11,13)
@@ -90,9 +218,10 @@ tft.fill(TFT.BLACK)
 drawAxes()
 
 sine = data.sineWave()
-sinePin = Pin(20, mode=Pin.OUT)
-sineDAC = PWM(sinePin)
-sineDAC.freq(1000000)
+tri = data.triWave()
+wavePin = Pin(20, mode=Pin.OUT)
+waveDAC = PWM(wavePin)
+waveDAC.freq(1000000)
 dacStep = 0
 
 C1Ref = ADC(29)
@@ -112,13 +241,24 @@ c4gnd.value(0)
 voltageFactor = 6.6/65535
 displayFactor = 8
 
+ADCDataC1 = [0] * 160
+ADCDataC2 = [0] * 160
+
 voltageDataC1 = [0] * 160
 voltageDataC2 = [0] * 160
 
 yValsC1 = [69] * 160
 yValsC2 = [69] * 160
 
+currentArrayIndex = 0
+plottingIndex = 160
 cursorPos = 0
+
+settings = [
+[0, ["sine", "triangle", "square"]],
+[0, ["volts", "watts"]],
+[0, ["immediate", "wave out peak"]]
+]
 
 leftButton = Pin(0, Pin.IN, Pin.PULL_UP)
 rightButton = Pin(1, Pin.IN, Pin.PULL_UP)
@@ -130,7 +270,7 @@ readySwitch = Pin(8, Pin.IN, Pin.PULL_UP)
 leftCounter  = 0
 rightCounter = 0
 modeCounter  = 0
-selectButton = 0
+selectCounter = 0
 readyCounter = 0
 startCounter = 0
 
@@ -145,12 +285,10 @@ if (not readySwitch.value()):
 elif (not startSwitch.value()):
     currentState = "start"
 
-start = 0
-
-dacTimer = Timer(mode=Timer.PERIODIC, period=1, callback=dacHandler)
-measurementTimer = Timer(mode=Timer.PERIODIC, period=10, callback=measurementHandler)
 measuring = False
 textUpdateCounter = 0
+
+startTimers()
 
 while(True):
     time.sleep(0.001)
@@ -178,6 +316,22 @@ while(True):
             rightPressed = True
     else:
         rightCounter = 0
+    
+    if (not modeButton.value()):
+        modeCounter += 1
+        if (modeCounter == 5):
+            modePressed = True
+    else:
+        modeCounter = 0
+        modePressed = False
+    
+    if (not selectButton.value()):
+        selectCounter += 1
+        if (modeCounter == 5):
+            selectPressed = True
+    else:
+        selectCounter = 0
+        selectPressed = False
     
     if (not readySwitch.value()):
         readyCounter += 1
@@ -210,14 +364,15 @@ while(True):
             else:
                 moveCursor(cursorPos + 1)
         
-        if(start):
+        if(modePressed):
+            modePressed = False
+            openMenu()
+        
+        if (currentSwitchState == "start" and lastSwitchState == "ready"):
             tft.fillrect((0,9), (160, 128), TFT.BLACK)
             drawAxes()
             cursorPos = 0
+            plottingIndex = 0
             measuring = True
-    
-    if (currentSwitchState != lastSwitchState):
-        if (currentSwitchState == "start" and lastSwitchState == "ready"):
-            start = True
-    lastSwitchState = currentSwitchState
+        lastSwitchState = currentSwitchState
 
